53667bb7edda7cb0c6f862e26a213e3d41002813
bwissal <baazizwissal13@gmail.com>
2024-03-06 11:53:54 +0100

    add role in the register form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -38,6 +38,17 @@
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
+<!-- Role Selection -->
+<div class="mt-4">
+    <x-input-label for="role" :value="__('Select Role')" />
+    <select id="role" name="role" class="block mt-1 w-full">
+        @foreach($roles as $role)
+            @if($role->name !== 'admin')
+                <option value="{{ $role->name }}">{{ $role->name }}</option>
+            @endif
+        @endforeach
+    </select>
+</div>
 
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
